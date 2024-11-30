4417c2ff281c1bcdeadc40b92b3b233f5c9cb539
sean <jh9826@jbnu.ac.kr>
2024-11-30 23:33:10 +0900

    refactor: @누락 수정

--- src/main/java/pretzel/dreamketcherbe/auth/jwt/JwtProvider.java
+++ src/main/java/pretzel/dreamketcherbe/auth/jwt/JwtProvider.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pretzel.dreamketcherbe.auth.dto.AuthPayload;
-import pretzel.dreamketcherbe.auth.utils.TokenProvider;
 
 import io.jsonwebtoken.security.Keys;
 import pretzel.dreamketcherbe.member.entity.Role;
@@ -15,7 +14,7 @@ import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
-Component
+@Component
 public class JwtProvider implements TokenProvider {
 
     private static final String MEMBER_ID = "memberId";
