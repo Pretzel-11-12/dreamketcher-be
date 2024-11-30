276acc7727ff1d864676722fb6a4ac27b7fafc2a
sean <jh9826@jbnu.ac.kr>
2024-11-30 22:54:12 +0900

    refactor: web config 패키지 위치 수정

--- src/main/java/pretzel/dreamketcherbe/common/config/WebConfig.java
+++ src/main/java/pretzel/dreamketcherbe/common/config/WebConfig.java
@@ -1,10 +1,12 @@
-package pretzel.dreamketcherbe.auth.config;
+package pretzel.dreamketcherbe.common.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pretzel.dreamketcherbe.auth.config.AuthArgumentResolver;
+import pretzel.dreamketcherbe.auth.config.AuthInterceptor;
 
 import java.util.List;
 
