22f9ba4278c9100a4cd75076666f2b50ca25a852
sean <jh9826@jbnu.ac.kr>
2024-11-30 22:51:14 +0900

    feat: Auth argument resolver

--- src/main/java/pretzel/dreamketcherbe/auth/config/WebConfig.java
+++ src/main/java/pretzel/dreamketcherbe/auth/config/WebConfig.java
@@ -2,14 +2,25 @@ package pretzel.dreamketcherbe.auth.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
+    private final AuthArgumentResolver authArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(
+        List<HandlerMethodArgumentResolver> resolvers
+    ) {
+        resolvers.add(authArgumentResolver);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
