b53be8c1acd92263098f3ec439e5c61adc32bf16
sean <jh9826@jbnu.ac.kr>
2024-12-01 21:18:25 +0900

    feat: AuthExceptionType 종류 추가

--- src/main/java/pretzel/dreamketcherbe/auth/exception/AuthExceptionType.java
+++ src/main/java/pretzel/dreamketcherbe/auth/exception/AuthExceptionType.java
@@ -4,7 +4,13 @@ import org.springframework.http.HttpStatus;
 import pretzel.dreamketcherbe.common.exception.ExceptionType;
 
 public enum AuthExceptionType implements ExceptionType {
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증 정보가 제공되지 않았습니다.");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증 정보가 제공되지 않았습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 리프레시 토큰입니다."),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증 정보입니다."),
+    INVALID_ROLE(HttpStatus.FORBIDDEN, "유효하지 않은 권한입니다.");
 
     private final HttpStatus status;
     private final String message;
