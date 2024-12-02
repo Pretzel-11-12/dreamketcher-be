077f11fd801989d803515d0c79eb63f1e0c399c9
ss0ming <hee9920@naver.com>
2024-12-02 23:54:40 +0900

    refactor: hashTag를 genre로 수정

--- src/main/java/pretzel/dreamketcherbe/domain/webtoon/exception/WebtoonExceptionType.java
+++ src/main/java/pretzel/dreamketcherbe/domain/webtoon/exception/WebtoonExceptionType.java
@@ -5,8 +5,8 @@ import pretzel.dreamketcherbe.common.exception.ExceptionType;
 
 public enum WebtoonExceptionType implements ExceptionType {
 
-    HASH_TAG_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 해시태그입니다."),
-    WEBTOON_HASH_TAG_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 웹툰 해시태그입니다.");
+    GENRE_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 장르입니다."),
+    WEBTOON_GENRE_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 웹툰 장르입니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/pretzel/dreamketcherbe/domain/webtoon/service/WebtoonService.java
+++ src/main/java/pretzel/dreamketcherbe/domain/webtoon/service/WebtoonService.java
@@ -3,11 +3,11 @@ package pretzel.dreamketcherbe.domain.webtoon.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pretzel.dreamketcherbe.domain.webtoon.dto.WebtoonResDto;
-import pretzel.dreamketcherbe.domain.webtoon.entity.HashTag;
+import pretzel.dreamketcherbe.domain.webtoon.entity.Genre;
 import pretzel.dreamketcherbe.domain.webtoon.exception.WebtoonException;
 import pretzel.dreamketcherbe.domain.webtoon.exception.WebtoonExceptionType;
-import pretzel.dreamketcherbe.domain.webtoon.repository.HashTagRepository;
-import pretzel.dreamketcherbe.domain.webtoon.repository.WebtoonHashTagRepository;
+import pretzel.dreamketcherbe.domain.webtoon.repository.GenreRepository;
+import pretzel.dreamketcherbe.domain.webtoon.repository.WebtoonGenreRepository;
 import pretzel.dreamketcherbe.domain.webtoon.repository.WebtoonRepository;
 
 import java.util.List;
@@ -19,17 +19,17 @@ public class WebtoonService {
 
     private final WebtoonRepository webtoonRepository;
 
-    private final WebtoonHashTagRepository webtoonHashTagRepository;
+    private final WebtoonGenreRepository webtoonGenreRepository;
 
-    private final HashTagRepository hashTagRepository;
+    private final GenreRepository GenreRepository;
 
-    public List<WebtoonResDto> getWebtoonsByGenre(final String genre) {
-        HashTag hashTag = hashTagRepository.findByName(genre)
-                .orElseThrow(() -> new WebtoonException(WebtoonExceptionType.HASH_TAG_NOT_FOUND));
+    public List<WebtoonResDto> getWebtoonsByGenre(final String genreName) {
+        Genre genre = GenreRepository.findByName(genreName)
+                .orElseThrow(() -> new WebtoonException(WebtoonExceptionType.GENRE_NOT_FOUND));
 
-        List<Long> webtoonIds = webtoonHashTagRepository.findByHashTagId(hashTag.getId())
+        List<Long> webtoonIds = webtoonGenreRepository.findByGenreId(genre.getId())
                 .stream()
-                .map(webtoonHashTag -> webtoonHashTag.getWebtoon().getId())
+                .map(webtoonGenre -> webtoonGenre.getWebtoon().getId())
                 .collect(Collectors.toList());
 
         return webtoonRepository.findAllById(webtoonIds)
