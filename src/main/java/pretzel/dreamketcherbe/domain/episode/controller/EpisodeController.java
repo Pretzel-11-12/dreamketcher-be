7f69489df3fb9def2b0f1fab40b0952abf68a852
dev-Rhea <dev.rhea4444@gmail.com>
2024-12-07 17:12:19 +0900

    feat: 에피소드 repository, service, controller 추가

---
+++ src/main/java/pretzel/dreamketcherbe/domain/episode/controller/EpisodeController.java
@@ -0,0 +1,14 @@
+package pretzel.dreamketcherbe.domain.episode.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/episodes")
+@AllArgsConstructor
+public class EpisodeController {
+
+}
