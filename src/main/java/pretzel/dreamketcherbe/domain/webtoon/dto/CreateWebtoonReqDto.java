073a0349eec3293eaa47764bfd462ec7c8db3c6b
dev-Rhea <dev.rhea4444@gmail.com>
2024-12-07 15:50:00 +0900

    refactor: 변수명 수정

--- src/main/java/pretzel/dreamketcherbe/domain/webtoon/dto/CreateWebtoonReqDto.java
+++ src/main/java/pretzel/dreamketcherbe/domain/webtoon/dto/CreateWebtoonReqDto.java
@@ -3,7 +3,7 @@ package pretzel.dreamketcherbe.domain.webtoon.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateWebtoonReqDto(
-    String name,
+    @NotBlank String member,
     @NotBlank String title,
     @NotBlank String thumbnail,
     @NotBlank String prologue,
