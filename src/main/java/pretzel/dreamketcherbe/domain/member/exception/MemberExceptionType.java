756ab548bf086bdb1bd773062e8a3c6f2e4fa17b
sean <jh9826@jbnu.ac.kr>
2024-12-08 20:47:38 +0900

    feat: 닉네임 중복 exception 추가

--- src/main/java/pretzel/dreamketcherbe/domain/member/exception/MemberExceptionType.java
+++ src/main/java/pretzel/dreamketcherbe/domain/member/exception/MemberExceptionType.java
@@ -5,7 +5,8 @@ import pretzel.dreamketcherbe.common.exception.ExceptionType;
 
 public enum MemberExceptionType implements ExceptionType {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
-    MEMBER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED, "해당 회원은 권한이 없습니다.");
+    MEMBER_NOT_AUTHORIZED(HttpStatus.UNAUTHORIZED, "해당 회원은 권한이 없습니다."),
+    NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다.");
 
     private final HttpStatus status;
     private final String message;
