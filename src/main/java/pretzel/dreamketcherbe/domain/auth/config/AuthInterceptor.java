0589ba67351883a49a0aba6acdfe9b1b7cfef01a
sean <jh9826@jbnu.ac.kr>
2024-12-09 01:24:33 +0900

    refactor: authinterceptor 범위 확장

--- src/main/java/pretzel/dreamketcherbe/domain/auth/config/AuthInterceptor.java
+++ src/main/java/pretzel/dreamketcherbe/domain/auth/config/AuthInterceptor.java
@@ -4,12 +4,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import pretzel.dreamketcherbe.common.annotation.Auth;
 import pretzel.dreamketcherbe.domain.auth.exception.AuthException;
 import pretzel.dreamketcherbe.domain.auth.exception.AuthExceptionType;
 import pretzel.dreamketcherbe.domain.auth.repository.TokenExtractor;
 import pretzel.dreamketcherbe.domain.auth.service.internal.AuthContext;
 import pretzel.dreamketcherbe.domain.auth.utils.AuthHeaderExtractor;
+
+import static java.util.Arrays.stream;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +28,18 @@ public class AuthInterceptor implements HandlerInterceptor {
         HttpServletResponse response,
         Object handler
     ) throws Exception {
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler;
+
+            boolean hasAuthAnnotation =
+                    stream(hm.getMethodParameters())
+                    .anyMatch(p -> p.hasParameterAnnotation(Auth.class));
+
+            if (!hasAuthAnnotation) {
+                return true;
+            }
+        }
 
         String token = AuthHeaderExtractor.extract(request)
             .orElseThrow(() -> new AuthException(AuthExceptionType.UNAUTHORIZED));
