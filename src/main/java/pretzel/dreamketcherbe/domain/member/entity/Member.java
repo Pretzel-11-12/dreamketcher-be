cf1261e6e278f51971f5e37b5da410b2b7f53a47
sean <jh9826@jbnu.ac.kr>
2024-12-09 23:34:48 +0900

    refactor: resloving comment 오타 수정

--- src/main/java/pretzel/dreamketcherbe/domain/member/entity/Member.java
+++ src/main/java/pretzel/dreamketcherbe/domain/member/entity/Member.java
@@ -41,7 +41,7 @@ public class Member extends BaseTimeEntity {
     private Role role;
 
     @Builder
-    public Member(SocialType socialType, String socialId, String email, String name,String nickname, String imageUri, Role role) {
+    public Member(SocialType socialType, String socialId, String email, String name, String nickname, String imageUri, Role role) {
         this.socialType = socialType;
         this.socialId = socialId;
         this.name = name;
