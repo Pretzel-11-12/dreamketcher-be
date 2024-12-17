50179c94d77320e9669a389a5f286e4601b09f65
dev-Rhea <dev.rhea4444@gmail.com>
2024-12-17 21:38:03 +0900

    chore: builder 사용

--- src/main/java/pretzel/dreamketcherbe/domain/episode/dto/CreateEpisodeLikeResDto.java
+++ src/main/java/pretzel/dreamketcherbe/domain/episode/dto/CreateEpisodeLikeResDto.java
@@ -1,11 +1,17 @@
 package pretzel.dreamketcherbe.domain.episode.dto;
 
+import lombok.Builder;
+
+@Builder
 public record CreateEpisodeLikeResDto(
     Long episodeId,
     int likeCount
 ) {
 
     public static CreateEpisodeLikeResDto of(Long episodeId, int likeCount) {
-        return new CreateEpisodeLikeResDto(episodeId, likeCount);
+        return CreateEpisodeLikeResDto.builder()
+            .episodeId(episodeId)
+            .likeCount(likeCount)
+            .build();
     }
 }
