e709f2c61e10972ecaba7ace02599859171c4c98
ss0ming <hee9920@naver.com>
2024-12-27 00:35:37 +0900

    feat: 전체/장르별 랭킹 목록 조회

--- src/main/java/pretzel/dreamketcherbe/domain/ranking/controller/RankingController.java
+++ src/main/java/pretzel/dreamketcherbe/domain/ranking/controller/RankingController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pretzel.dreamketcherbe.domain.ranking.dto.RankingResDto;
 import pretzel.dreamketcherbe.domain.ranking.service.RankingService;
@@ -18,10 +19,18 @@ public class RankingController {
     private final RankingService rankingService;
 
     /**
-     * 랭킹 조회
+     * 전체 랭킹 목록 조회
      */
     @GetMapping
-    public ResponseEntity<List<RankingResDto>> getRanking() {
-        return ResponseEntity.ok(rankingService.getRanking());
+    public ResponseEntity<List<RankingResDto>> getAllRanking() {
+        return ResponseEntity.ok(rankingService.getAllRanking());
+    }
+
+    /**
+     * 전체 + 장르 랭킹 목록 조회
+     */
+    @GetMapping("/genre")
+    public ResponseEntity<List<RankingResDto>> getAllRankingByGenre(@RequestParam String genre) {
+        return ResponseEntity.ok(rankingService.getAllRankingByGenre(genre));
     }
 }
--- src/main/java/pretzel/dreamketcherbe/domain/ranking/service/RankingService.java
+++ src/main/java/pretzel/dreamketcherbe/domain/ranking/service/RankingService.java
@@ -15,15 +15,42 @@ public class RankingService {
 
     private final WebtoonRepository webtoonRepository;
 
-    public List<RankingResDto> getRanking() {
-        return getWebtoonPopularityDataDto().stream()
-                .map(RankingResDto::of)
+    /**
+     * 전체 랭킹 목록 조회
+     */
+    public List<RankingResDto> getAllRanking() {
+        return getAllPopularityDataDto().stream()
+            .map(RankingResDto::of)
+            .toList();
+    }
+
+    /**
+     * 전체 + 장르 랭킹 목록 조회
+     */
+    public List<RankingResDto> getAllRankingByGenre(String genre) {
+        return getAllPopularityDataDtoByGenre(genre).stream()
+            .map(RankingResDto::of)
+            .toList();
+    }
+
+    /**
+     * 전체 랭킹 인기 dto 가져오는 메서드
+     */
+    private List<WebtoonPopularityDataDto> getAllPopularityDataDto() {
+        return webtoonRepository.findAllWithPopularityData().stream()
+                .map(WebtoonPopularityDataDto::of)
+                .sorted(Comparator.comparing(WebtoonPopularityDataDto::getPopularity).reversed())
+                .limit(12)
                 .toList();
     }
 
-    private List<WebtoonPopularityDataDto> getWebtoonPopularityDataDto() {
+    /**
+     * 전체 + 장르 인기 dto 가져오는 메서드
+     */
+    private List<WebtoonPopularityDataDto> getAllPopularityDataDtoByGenre(String genre) {
         return webtoonRepository.findAllWithPopularityData().stream()
                 .map(WebtoonPopularityDataDto::of)
+                .filter(webtoon -> webtoon.getGenres().contains(genre))
                 .sorted(Comparator.comparing(WebtoonPopularityDataDto::getPopularity).reversed())
                 .limit(12)
                 .toList();
