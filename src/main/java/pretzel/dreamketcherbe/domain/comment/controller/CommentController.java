3ad866c7cd9cbe66503cd57c86e7ef1bd95ca1c1
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-01 21:40:33 +0900

    feat: 댓글 목록 조회 기능 추가

--- src/main/java/pretzel/dreamketcherbe/domain/comment/controller/CommentController.java
+++ src/main/java/pretzel/dreamketcherbe/domain/comment/controller/CommentController.java
@@ -1,17 +1,24 @@
 package pretzel.dreamketcherbe.domain.comment.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.query.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pretzel.dreamketcherbe.common.annotation.Auth;
+import pretzel.dreamketcherbe.common.dto.PageReqDto;
+import pretzel.dreamketcherbe.common.dto.PageResDto;
+import pretzel.dreamketcherbe.domain.comment.dto.CommentResDto;
 import pretzel.dreamketcherbe.domain.comment.dto.CreateCommentReqDto;
 import pretzel.dreamketcherbe.domain.comment.dto.CreateCommentResDto;
 import pretzel.dreamketcherbe.domain.comment.dto.CreateRecommentReqDto;
@@ -50,6 +57,21 @@ public class CommentController {
         @PathVariable Long commentId) {
         commentService.deleteComment(memberId, episodeId, commentId);
         return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * 댓글 목록 조회
+     */
+    @GetMapping
+    public ResponseEntity<PageResDto<CommentResDto>> getComments(@PathVariable Long episodeId,
+        @RequestParam int page,
+        @RequestParam int size, @RequestParam(defaultValue = "DESC") String order) {
+
+        PageReqDto pageReqDto = PageReqDto.of(null, page, size, order);
+
+        PageResDto<CommentResDto> response = commentService.getComments(episodeId, pageReqDto);
+
+        return ResponseEntity.ok(response);
     }
 
     /**
--- src/main/java/pretzel/dreamketcherbe/domain/comment/repository/CommentRepository.java
+++ src/main/java/pretzel/dreamketcherbe/domain/comment/repository/CommentRepository.java
@@ -1,8 +1,13 @@
 package pretzel.dreamketcherbe.domain.comment.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pretzel.dreamketcherbe.domain.comment.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @Query("SELECT c FROM Comment c WHERE c.episode.id = :episodeId AND c.isDeleted = false ")
+    Page<Comment> findByEpisodeId(Long episodeId, Pageable pageables);
 }
