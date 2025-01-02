bbef4d3e6c55da74036a09042937f394dca85663
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-02 21:21:34 +0900

    refactor: 미사용 import문 삭제

--- src/main/java/pretzel/dreamketcherbe/domain/episode/repository/EpisodeLikeRepository.java
+++ src/main/java/pretzel/dreamketcherbe/domain/episode/repository/EpisodeLikeRepository.java
@@ -1,7 +1,6 @@
 package pretzel.dreamketcherbe.domain.episode.repository;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
