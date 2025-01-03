94148961b5d3672fe73a03bc493b62df3ec4ccc4
ss0ming <hee9920@naver.com>
2025-01-03 18:05:28 +0900

    feat: 작가 자신의 작품 목록 조회 기능

--- src/main/java/pretzel/dreamketcherbe/domain/member/repository/MemberRepository.java
+++ src/main/java/pretzel/dreamketcherbe/domain/member/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package pretzel.dreamketcherbe.domain.member.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pretzel.dreamketcherbe.domain.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     Optional<Member> findBySocialId(String socialId);
 
