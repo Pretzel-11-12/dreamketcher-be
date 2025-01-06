6e1932d962bfd26dde8e33688fef4eae89e577ec
ss0ming <hee9920@naver.com>
2025-01-06 15:39:11 +0900

    feat: 작품 승인 변경 기능

--- src/main/java/pretzel/dreamketcherbe/domain/admin/exception/AdminExceptionType.java
+++ src/main/java/pretzel/dreamketcherbe/domain/admin/exception/AdminExceptionType.java
@@ -5,7 +5,9 @@ import pretzel.dreamketcherbe.common.exception.ExceptionType;
 
 public enum AdminExceptionType implements ExceptionType {
     MANAGE_WEBTOON_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 웹툰 관리를 찾을 수 없습니다."),
-    MANAGE_EPISODE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회차 관리를 찾을 수 없습니다.");
+    MANAGE_EPISODE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회차 관리를 찾을 수 없습니다."),
+    APPROVAL_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 승인 타입은 찾을 수 없습니다."),
+    REASON_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 이유는 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
