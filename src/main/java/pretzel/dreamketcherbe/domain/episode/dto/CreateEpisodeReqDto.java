0e060f8f5fe37febd59bc1cd8ee0211cd30d23f3
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-07 12:24:21 +0900

    fix: 유효성 검사 수정

--- src/main/java/pretzel/dreamketcherbe/domain/episode/dto/CreateEpisodeReqDto.java
+++ src/main/java/pretzel/dreamketcherbe/domain/episode/dto/CreateEpisodeReqDto.java
@@ -1,6 +1,7 @@
 package pretzel.dreamketcherbe.domain.episode.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
@@ -8,8 +9,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 public record CreateEpisodeReqDto(
     @NotBlank String title,
-    @NotBlank String thumbnail,
-    @NotBlank List<String> content,
+    @NotNull String thumbnail,
+    @NotEmpty List<String> content,
     @NotBlank String authorNote,
     @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate publishedAt
 ) {
