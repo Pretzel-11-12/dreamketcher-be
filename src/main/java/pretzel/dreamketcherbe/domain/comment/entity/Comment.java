443ca9a12655448924fe5666dd1d7570115cb085
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-07 16:36:59 +0900

    chore: entity 수정

--- src/main/java/pretzel/dreamketcherbe/domain/comment/entity/Comment.java
+++ src/main/java/pretzel/dreamketcherbe/domain/comment/entity/Comment.java
@@ -20,6 +20,7 @@ import org.hibernate.annotations.SQLRestriction;
 import pretzel.dreamketcherbe.common.entity.BaseTimeEntity;
 import pretzel.dreamketcherbe.domain.episode.entity.Episode;
 import pretzel.dreamketcherbe.domain.member.entity.Member;
+import pretzel.dreamketcherbe.domain.webtoon.entity.Webtoon;
 
 @Table(name = "comments")
 @Getter
@@ -53,9 +54,6 @@ public class Comment extends BaseTimeEntity {
     @ColumnDefault("false")
     private boolean isDeleted;
 
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
-
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
@@ -64,10 +62,18 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "episode_id")
     private Episode episode;
 
+    @ManyToOne
+    @JoinColumn(name = "webtoon_id")
+    private Webtoon webtoon;
+
     @Builder
-    public Comment(String content, int recommendationCount, Member member, Episode episode) {
+    public Comment(String content, int childCommentCount, int recommendationCount,
+        int notRecommendationCount,
+        Member member, Episode episode) {
         this.content = content;
+        this.childCommentCount = childCommentCount;
         this.recommendationCount = recommendationCount;
+        this.notRecommendationCount = notRecommendationCount;
         this.member = member;
         this.episode = episode;
     }
@@ -80,7 +86,6 @@ public class Comment extends BaseTimeEntity {
 
     public void softDelete() {
         this.isDeleted = true;
-        this.deletedAt = LocalDateTime.now();
     }
 
     public void updateChildCommentCount(int count) {
