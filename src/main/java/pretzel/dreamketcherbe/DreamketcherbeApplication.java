f91690dfb3a993cd15c199b741b5bdbac981d87a
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-14 18:48:11 +0900

    feature: scheduling 관련 설정 추가

--- src/main/java/pretzel/dreamketcherbe/DreamketcherbeApplication.java
+++ src/main/java/pretzel/dreamketcherbe/DreamketcherbeApplication.java
@@ -8,8 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableScheduling
-@EnableAsync
 public class DreamketcherbeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/pretzel/dreamketcherbe/common/config/SchedulerConfig.java
@@ -0,0 +1,34 @@
+package pretzel.dreamketcherbe.common.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+@EnableAsync
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    private final int POOL_SIZE = 10;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.initialize();
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(POOL_SIZE);
+    }
+
+}
