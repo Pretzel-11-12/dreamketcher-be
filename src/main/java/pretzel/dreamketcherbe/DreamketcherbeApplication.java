b690b0f85ba12697603151625144266d757ff779
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-14 18:14:07 +0900

    feature: scheduling 어노테이션 추가

--- src/main/java/pretzel/dreamketcherbe/DreamketcherbeApplication.java
+++ src/main/java/pretzel/dreamketcherbe/DreamketcherbeApplication.java
@@ -3,9 +3,13 @@ package pretzel.dreamketcherbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
+@EnableAsync
 public class DreamketcherbeApplication {
 
     public static void main(String[] args) {
