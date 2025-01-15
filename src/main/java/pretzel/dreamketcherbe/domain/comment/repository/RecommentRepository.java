a8847233cb019b2c4442df30e6d933821bccff4b
dev-Rhea <dev.rhea4444@gmail.com>
2025-01-15 12:36:44 +0900

    refactor: 미사용 import문 삭제

--- src/main/java/pretzel/dreamketcherbe/domain/comment/repository/RecommentRepository.java
+++ src/main/java/pretzel/dreamketcherbe/domain/comment/repository/RecommentRepository.java
@@ -1,6 +1,5 @@
 package pretzel.dreamketcherbe.domain.comment.repository;
 
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
