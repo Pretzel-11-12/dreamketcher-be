6610d9e4463e4ca6ffbed1a6ee3e07edbb5b3373
ss0ming <hee9920@naver.com>
2025-02-11 18:27:02 +0900

    feat: 랭킹 조회 및 작가 자신의 작품 조회 수정 - description 삭제 - story로 통합

--- src/main/java/pretzel/dreamketcherbe/domain/ranking/controller/RankingController.java
+++ src/main/java/pretzel/dreamketcherbe/domain/ranking/controller/RankingController.java
@@ -19,26 +19,27 @@ public class RankingController {
     private final RankingService rankingService;
 
     /**
-     * 전체 랭킹 목록 조회
+     * 랭킹 목록 조회
      */
     @GetMapping
-    public ResponseEntity<List<RankingResDto>> getAllRanking(@RequestParam(defaultValue = "none") String genre) {
-        return ResponseEntity.ok(rankingService.getAllRanking(genre));
+    public ResponseEntity<List<RankingResDto>> getAllRanking(@RequestParam(defaultValue = "none") String genre,
+                                                             @RequestParam(defaultValue = "none") String status) {
+        return ResponseEntity.ok(rankingService.getRanking(genre, status));
     }
 
-    /**
-     * 신작 랭킹 목록 조회
-     */
-    @GetMapping("/new")
-    public ResponseEntity<List<RankingResDto>> getNewRanking(@RequestParam(defaultValue = "none") String genre) {
-        return ResponseEntity.ok(rankingService.getNewRanking(genre));
-    }
-
-    /**
-     * 완결 랭킹 목록 조회
-     */
-    @GetMapping("/finish")
-    public ResponseEntity<List<RankingResDto>> getFinishRanking(@RequestParam(defaultValue = "none") String genre) {
-        return ResponseEntity.ok(rankingService.getFinishRanking(genre));
-    }
+//    /**
+//     * 신작 랭킹 목록 조회
+//     */
+//    @GetMapping("/new")
+//    public ResponseEntity<List<RankingResDto>> getNewRanking(@RequestParam(defaultValue = "none") String genre) {
+//        return ResponseEntity.ok(rankingService.getNewRanking(genre));
+//    }
+//
+//    /**
+//     * 완결 랭킹 목록 조회
+//     */
+//    @GetMapping("/finish")
+//    public ResponseEntity<List<RankingResDto>> getFinishRanking(@RequestParam(defaultValue = "none") String genre) {
+//        return ResponseEntity.ok(rankingService.getFinishRanking(genre));
+//    }
 }
--- src/main/java/pretzel/dreamketcherbe/domain/ranking/dto/WebtoonPopularityDataDto.java
+++ src/main/java/pretzel/dreamketcherbe/domain/ranking/dto/WebtoonPopularityDataDto.java
@@ -12,7 +12,7 @@ public class WebtoonPopularityDataDto {
     private Long id;
     private String title;
     private String member;
-    private String description;
+    private String story;
     private String thumbnail;
     private String genre;
     private int lastEpisode;
@@ -23,13 +23,13 @@ public class WebtoonPopularityDataDto {
     private Long interestedCount;
     private float popularity;
 
-    public WebtoonPopularityDataDto(Long id, String title, String member, String description, String thumbnail, String genre, int lastEpisode,
+    public WebtoonPopularityDataDto(Long id, String title, String member, String story, String thumbnail, String genre, int lastEpisode,
                                     float averageStar, Long numOfStars, Long likeCount, Long viewCount,
                                     Long interestedCount, float popularity) {
         this.id = id;
         this.title = title;
         this.member = member;
-        this.description = description;
+        this.story = story;
         this.thumbnail = thumbnail;
         this.genre = genre;
         this.lastEpisode = lastEpisode;
