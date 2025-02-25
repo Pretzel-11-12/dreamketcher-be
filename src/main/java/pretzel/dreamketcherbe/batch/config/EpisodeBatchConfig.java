a3a2101f0662a5e22830e0f35a7754a2019d20d5
dev-Rhea <dev.rhea4444@gmail.com>
2025-02-25 17:21:12 +0900

    fix: 쿼리 수정, dto, entity 파라미터 수정

--- src/main/java/pretzel/dreamketcherbe/batch/config/EpisodeBatchConfig.java
+++ src/main/java/pretzel/dreamketcherbe/batch/config/EpisodeBatchConfig.java
@@ -1,19 +1,18 @@
 package pretzel.dreamketcherbe.batch.config;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
@@ -23,8 +22,12 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import pretzel.dreamketcherbe.domain.episode.dto.BatchEpisodeDto;
 import pretzel.dreamketcherbe.domain.episode.entity.Episode;
+import pretzel.dreamketcherbe.domain.episode.exception.EpisodeException;
+import pretzel.dreamketcherbe.domain.episode.exception.EpisodeExceptionType;
 import pretzel.dreamketcherbe.domain.episode.repository.EpisodeRepository;
 
+
+@Slf4j
 @Configuration
 @AllArgsConstructor
 public class EpisodeBatchConfig {
@@ -35,19 +38,23 @@ public class EpisodeBatchConfig {
     /**
      * 미발행 에피소드 읽기
      */
-    @StepScope
     @Bean
-    public JpaPagingItemReader<BatchEpisodeDto> episodeItemReader() {
+    public JpaPagingItemReader<Episode> episodeItemReader() {
 
         if (entityManagerFactoryBean.getObject() == null) {
             throw new IllegalStateException("entity manager factory been 이 null 입니다.");
         }
 
-        return new JpaPagingItemReaderBuilder<BatchEpisodeDto>()
+        return new JpaPagingItemReaderBuilder<Episode>()
             .name("episodeItemReader")
             .entityManagerFactory(entityManagerFactoryBean.getObject())
             .queryString(
-                "SELECT e FROM episodes e WHERE e.publishedAt = :today AND e.published = false AND e.status = 'APPROVAL'")
+                "SELECT e FROM Episode e " +
+                    "JOIN FETCH e.webtoon w " +
+                    "LEFT JOIN FETCH w.member m " +
+                    "WHERE FUNCTION('DATE', e.publishedAt) = :today " +
+                    "AND e.published = false " +
+                    "AND e.status = 'APPROVAL'")
             .parameterValues(Map.of("today", LocalDate.now()))
             .pageSize(10)
             .build();
@@ -56,53 +63,66 @@ public class EpisodeBatchConfig {
     /**
      * published 상태 업데이트
      */
-    @StepScope
     @Bean
-    public ItemProcessor<BatchEpisodeDto, BatchEpisodeDto> episodeEpisodeItemProcessor() {
-        return dto -> new BatchEpisodeDto(
-            dto.id(),
-            dto.no(),
-            dto.webtoonTitle(),
-            dto.title(),
-            dto.thumbnail(),
-            dto.content(),
-            dto.authorName(),
-            dto.authorNote(),
-            dto.authorImage(),
-            dto.publishedAt(),
-            true,
-            dto.likeCount(),
-            dto.viewCount(),
-            dto.averageStar()
+    public ItemProcessor<Episode, BatchEpisodeDto> episodeEpisodeItemProcessor() {
+
+        return episode -> new BatchEpisodeDto(
+            episode.getId(),
+            episode.getNo(),
+            episode.getWebtoon().getTitle(),
+            episode.getTitle(),
+            episode.getThumbnail(),
+            episode.getContent(),
+            episode.getWebtoon().getMember().getName(),
+            episode.getAuthorNote(),
+            episode.getMember().getImageUrl(),
+            episode.getPublishedAt(),
+            true,  // published 상태 업데이트
+            episode.getLikeCount(),
+            episode.getViewCount(),
+            episode.getAverageStar()
         );
     }
 
     /**
      * 에피소드 업데이트 저장
      */
-    @StepScope
     @Bean
     public ItemWriter<BatchEpisodeDto> episodeItemWriter(EpisodeRepository episodeRepository) {
-        return items -> items.forEach(dto -> {
-            Episode episode = episodeRepository.findById(dto.id())
-                .orElseThrow(
-                    () -> new IllegalStateException("Episode not found with id: " + dto.id()));
-            episode.setPublished(dto.published());
-            episodeRepository.save(episode);
-        });
+        return items -> {
+            if (items.isEmpty()) {
+                log.warn("아이템이 존재하지 않습니다.");
+                return;
+            }
+
+            List<Long> episodeIds = items.getItems().stream().map(BatchEpisodeDto::id).toList();
+            List<Episode> episodes = episodeRepository.findAllById(episodeIds);
+
+            Map<Long, Episode> episodeMap = episodes.stream()
+                .collect(Collectors.toMap(Episode::getId, e -> e));
+
+            items.forEach(dto -> {
+                Episode episode = episodeMap.get(dto.id());
+                if (episode == null) {
+                    throw new EpisodeException(EpisodeExceptionType.EPISODE_NOT_FOUND);
+                }
+                episode.updatePublished(dto.published());
+            });
+            episodeRepository.saveAll(episodes);
+        };
     }
+
 
     /**
      * Step : Chunk 기반 처리
      */
-    @JobScope
     @Bean
     public Step episodeStep(JobRepository jobRepository,
-        ItemReader<BatchEpisodeDto> reader,
-        ItemProcessor<BatchEpisodeDto, BatchEpisodeDto> processor,
+        JpaPagingItemReader<Episode> reader,
+        ItemProcessor<Episode, BatchEpisodeDto> processor,
         ItemWriter<BatchEpisodeDto> writer) {
         return new StepBuilder("episodeStep", jobRepository)
-            .<BatchEpisodeDto, BatchEpisodeDto>chunk(10, transactionManager)
+            .<Episode, BatchEpisodeDto>chunk(10, transactionManager)
             .reader(reader)
             .processor(processor)
             .writer(writer)
@@ -115,8 +135,8 @@ public class EpisodeBatchConfig {
     @Bean
     public Job episodeJob(JobRepository jobRepository, Step episodeStep) {
         return new JobBuilder("episodeJob", jobRepository)
+            .incrementer(new RunIdIncrementer()) // 실행 마다 새로운 JobInstance 생성
             .start(episodeStep)
             .build();
     }
-
 }
