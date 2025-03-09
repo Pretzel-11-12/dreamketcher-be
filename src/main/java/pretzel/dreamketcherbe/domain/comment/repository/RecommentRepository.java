254aa3d91a56ef36322c141254f4d8a6926c0021
dev-Rhea <dev.rhea4444@gmail.com>
2025-03-09 19:10:17 +0900

    fix: 쿼리 수정

--- src/main/java/pretzel/dreamketcherbe/domain/comment/repository/RecommentRepository.java
+++ src/main/java/pretzel/dreamketcherbe/domain/comment/repository/RecommentRepository.java
@@ -20,7 +20,7 @@ public interface RecommentRepository extends JpaRepository<Recomment, Long> {
     @Query("SELECT r.id FROM Recomment r")
     List<Long> findAllRecommentIds();
 
-    @Query("SELECT r.id FROM Recomment r WHERE r.member.id = :memberId AND r.isDeleted = false")
+    @Query("SELECT r FROM Recomment r WHERE r.member.id = :memberId AND r.isDeleted = false")
     Page<Recomment> findByMemberIdAndIsDeletedFalse(Long memberId, Pageable pageable);
 
     @Query("SELECT r.id FROM Recomment r WHERE r.comment.id = :commentId AND r.isDeleted = false")
