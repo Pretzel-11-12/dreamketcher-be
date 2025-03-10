9cb19b9bef61db689eb76aca300e0f8eb346fb16
sean <jh9826@jbnu.ac.kr>
2025-03-10 15:06:01 +0900

    feat: mdc loggingfilter 추가

--- src/main/java/pretzel/dreamketcherbe/common/config/LoggingConfig.java
+++ src/main/java/pretzel/dreamketcherbe/common/config/LoggingConfig.java
@@ -1,9 +1,14 @@
 package pretzel.dreamketcherbe.common.config;
 
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pretzel.dreamketcherbe.common.logging.MdcLoggingFilter;
 import pretzel.dreamketcherbe.common.logging.QueryCounterInterceptor;
 
 @Configuration
@@ -17,5 +22,17 @@ public class LoggingConfig implements WebMvcConfigurer {
         registry.addInterceptor(queryCounterInterceptor)
             .addPathPatterns("/**")
             .order(0);
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> mdcLoggingFilter() {
+        FilterRegistrationBean<Filter> registrationBean =
+            new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new MdcLoggingFilter());
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registrationBean.addUrlPatterns("/*");
+
+        return registrationBean;
     }
 }
