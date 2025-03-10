0ad383179003eb33042a049393e7b5e562f9ed75
sean <jh9826@jbnu.ac.kr>
2025-03-10 15:32:45 +0900

    feat: request logging filter 추가

--- src/main/java/pretzel/dreamketcherbe/common/config/LoggingConfig.java
+++ src/main/java/pretzel/dreamketcherbe/common/config/LoggingConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pretzel.dreamketcherbe.common.logging.MdcLoggingFilter;
 import pretzel.dreamketcherbe.common.logging.QueryCounterInterceptor;
+import pretzel.dreamketcherbe.common.logging.RequestLoggingFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,6 +32,18 @@ public class LoggingConfig implements WebMvcConfigurer {
 
         registrationBean.setFilter(new MdcLoggingFilter());
         registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registrationBean.addUrlPatterns("/*");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> filterRegistration() {
+        FilterRegistrationBean<Filter> registrationBean =
+            new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new RequestLoggingFilter());
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
         registrationBean.addUrlPatterns("/*");
 
         return registrationBean;
