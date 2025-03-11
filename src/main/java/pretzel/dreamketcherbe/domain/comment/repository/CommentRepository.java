a2fb4e0eedccdd4cfa9cc7404dcae7f4881387ef
dev-Rhea <dev.rhea4444@gmail.com>
2025-03-11 22:03:51 +0900

    fix: 페이징 사이즈 통합 적용

--- src/main/java/pretzel/dreamketcherbe/domain/comment/repository/CommentRepository.java
+++ src/main/java/pretzel/dreamketcherbe/domain/comment/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package pretzel.dreamketcherbe.domain.comment.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -18,7 +19,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Long> findAllCommentIds();
 
     @Query("SELECT c FROM Comment c WHERE c.member.id = :memberId AND c.isDeleted = false")
-    Page<Comment> findByMemberIdAndDeletedFalse(Long memberId, Pageable pageable);
+    List<Comment> findByMemberIdAndDeletedFalse(@Param("memberId") Long memberId, Sort sort);
 
     @Query("SELECT c.id FROM Comment c WHERE c.episode.id IN :episodeIds AND c.isDeleted = false")
     List<Long> findByEpisodeId(@Param("episodeIds") List<Long> episodeIds);
