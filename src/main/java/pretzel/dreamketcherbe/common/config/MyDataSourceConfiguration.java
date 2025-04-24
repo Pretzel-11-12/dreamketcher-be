903645ba7b76829113806f3e6f89f84844573346
sean <wnsgh9826@naver.com>
2025-04-24 15:10:24 +0900

    feat: Change hikaripool settings

---
+++ src/main/java/pretzel/dreamketcherbe/common/config/MyDataSourceConfiguration.java
@@ -0,0 +1,28 @@
+package pretzel.dreamketcherbe.common.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class MyDataSourceConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder()
+            .type(HikariDataSource.class)
+            .build();
+    }
+
+}
