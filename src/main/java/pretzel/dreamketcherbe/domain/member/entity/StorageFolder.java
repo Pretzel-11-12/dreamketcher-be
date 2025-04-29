9636182bf7c07d2a36844301d0bb6a5b16c7f0e9
ss0ming <hee9920@naver.com>
2025-04-29 12:56:04 +0900

    feat: storage folder 엔티티 추가

---
+++ src/main/java/pretzel/dreamketcherbe/domain/member/entity/StorageFolder.java
@@ -0,0 +1,45 @@
+package pretzel.dreamketcherbe.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import pretzel.dreamketcherbe.common.entity.BaseTimeEntity;
+
+@Entity
+@Getter
+@SQLDelete(sql = "UPDATE storage_folder SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StorageFolder extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, name = "is_deleted")
+    @ColumnDefault("false")
+    private boolean isDeleted;
+
+    @Builder
+    private StorageFolder(String name, Member member) {
+        this.name = name;
+        this.member = member;
+    }
+}
