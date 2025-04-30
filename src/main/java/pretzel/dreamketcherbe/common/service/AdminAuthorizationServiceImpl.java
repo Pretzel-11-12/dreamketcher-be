b5446371ac3db86b490fadfd206f1a845e1b5f47
dev-Rhea <dev.rhea4444@gmail.com>
2025-04-30 22:13:29 +0900

    chore: 오타 수정

--- src/main/java/pretzel/dreamketcherbe/common/service/AdminAuthorizationServiceImpl.java
+++ src/main/java/pretzel/dreamketcherbe/common/service/AdminAuthorizationServiceImpl.java
@@ -11,10 +11,9 @@ public class AdminAuthorizationServiceImpl implements AdminAuthorizationService 
     private final MemberRepository memberRepository;
 
     @Override
-    public boolean isAmdin(Long memberId) {
+    public boolean isAdmin(Long memberId) {
         return memberRepository.findById(memberId)
             .map(member -> "ADMIN".equals(member.getRole()))
             .orElse(false);
     }
-
 }
