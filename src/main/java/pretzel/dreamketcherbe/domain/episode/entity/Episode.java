4a78dc85c02c7eec5b1f4fec0d8725aa5d870891
dev-Rhea <dev.rhea4444@gmail.com>
2025-04-30 19:59:36 +0900

    refactor: 미사용 import 문 삭제

--- src/main/java/pretzel/dreamketcherbe/domain/episode/entity/Episode.java
+++ src/main/java/pretzel/dreamketcherbe/domain/episode/entity/Episode.java
@@ -3,7 +3,6 @@ package pretzel.dreamketcherbe.domain.episode.entity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
