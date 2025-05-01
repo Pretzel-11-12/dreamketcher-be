422036c8eb8cad9c48192e6a2990c2dfc7b2306b
dev-Rhea <dev.rhea4444@gmail.com>
2025-05-01 17:46:22 +0900

    chore: 클래스명 변경

--- src/main/java/pretzel/dreamketcherbe/wordfilter/controller/WordController.java
+++ src/main/java/pretzel/dreamketcherbe/wordfilter/controller/WordController.java
@@ -1,7 +1,6 @@
 package pretzel.dreamketcherbe.wordfilter.controller;
 
 import java.util.List;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +20,7 @@ import pretzel.dreamketcherbe.wordfilter.service.WordService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/word-filter")
-public class Controller {
+public class WordController {
 
     private final WordService wordService;
 
