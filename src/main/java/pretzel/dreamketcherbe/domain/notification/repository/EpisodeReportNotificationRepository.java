d99e09624194e649f8cd8ca5ec1f35fd3e383966
dev-Rhea <dev.rhea4444@gmail.com>
2025-05-23 21:22:16 +0900

    feat: 알림 기능 도입

---
+++ src/main/java/pretzel/dreamketcherbe/domain/notification/repository/EpisodeReportNotificationRepository.java
@@ -0,0 +1,10 @@
+package pretzel.dreamketcherbe.domain.notification.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pretzel.dreamketcherbe.domain.notification.entity.EpisodeReportNotification;
+
+public interface EpisodeReportNotificationRepository extends
+    JpaRepository<EpisodeReportNotification, Long> {
+
+
+}
